58308389a1915ebad848ab5fd5575d94ecc3e850
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2025-05-28 11:47:40 +0700

    add splash screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/splashscreen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fresh Milkfish App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const Splashscreen(),
+    );
+  }
+}
