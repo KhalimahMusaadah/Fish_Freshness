d89d87843c9f243579a62cf270ea749c63b6a41c
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2025-06-02 10:29:59 +0700

    add dependensi & ml_helper

---
+++ lib/utils/ml_helper.dart
@@ -0,0 +1,11 @@
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+class MLHelper {
+  static Future<String> classifyImage(File image) async {
+    // buat implementasi ML
+    
+    // Contoh dummy 
+    await Future.delayed(const Duration(seconds: 1));
+    return ['Segar', 'Tidak Segar', 'Kurang Segar'][DateTime.now().second % 3];
+  }
+}
