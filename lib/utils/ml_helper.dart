0a2b2c583e7fe1b856cf6de177ee1552c586797b
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2025-06-03 12:34:19 +0700

    edit ml_helper.dart

--- lib/utils/ml_helper.dart
+++ lib/utils/ml_helper.dart
@@ -1,8 +1,25 @@
+import 'dart:ffi';
 import 'dart:io';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
 class MLHelper {
+  static late Interpreter _interpreter;
+  static late ImageProcessor _imageProcessor;
+  static bool _isInitialized = false;
+
+  //menginisialisasi model
+  static Future<void> initialize() async{
+    if (_isInitialized) return;
+
+    //load modelnya
+    _interpreter = await Interpreter.fromAsset('assets/models/model.tflite');
+
+    //Setup image processor
+  }
+
   static Future<String> classifyImage(File image) async {
-    // buat implementasi ML
+    if (_isInitialized) return;
     
     // Contoh dummy 
     await Future.delayed(const Duration(seconds: 1));
